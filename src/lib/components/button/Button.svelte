0b6937b0902b014e2997fa4499be8c7a7db1058f
Lucas Alexandre Fell <lucasafell@gmail.com>
2023-01-14 16:07:20 -0300

    feat: add provider buttons

--- src/lib/components/button/Button.svelte
+++ src/lib/components/button/Button.svelte
@@ -11,39 +11,46 @@
 
 	const isExternal = to ? !/^(\/|#)(.*)/g.test(to) : false;
 
-	let buttonStyles = cva('dark:brightness-75 transition-all inline-flex items-center gap-1', {
-		variants: {
-			intent: {
-				primary: 'btn btn-primary',
-				secondary: 'btn btn-secondary',
-				accent: 'btn btn-accent',
-				ghost: 'btn btn-ghost',
-				link: 'btn btn-link',
-				text: 'hover:opacity-70 text-neutral-content',
-				'text-base': 'hover:opacity-70 text-base-content',
-				'no-style': 'hover:scale-95',
-				danger: 'btn btn-error hover:bg-red-300 hover:border-red-300'
+	const buttonStyles = cva(
+		'transition-all h-[2.5rem] min-h-[2.5rem] inline-flex items-center gap-1',
+		{
+			variants: {
+				intent: {
+					primary: 'btn btn-primary ',
+					secondary: 'btn btn-secondary',
+					accent: 'btn btn-accent',
+					ghost: 'btn btn-ghost',
+					link: 'btn btn-link',
+					text: 'hover:opacity-70 text-neutral-content',
+					'text-base': 'hover:opacity-70 text-base-content',
+					'no-style': 'hover:scale-95',
+					danger: 'btn btn-error hover:bg-red-300 hover:border-red-300',
+					provider: 'btn normal-case hover:opacity-70 text-[0.95rem] w-full'
+				},
+				provider: {
+					google: 'bg-white text-black hover:bg-white'
+				},
+				disabled: {
+					true: 'cursor-not-allowed'
+				},
+				icon: {
+					true: 'text-accent hover:bg-accent/20 btn-circle'
+				},
+				isLoading: {
+					true: 'flex gap-2'
+				},
+				animated: {
+					true: 'hover:-translate-y-0.5'
+				},
+				size: {
+					sm: 'btn-sm'
+				}
 			},
-			disabled: {
-				true: 'cursor-not-allowed'
-			},
-			icon: {
-				true: 'text-accent hover:bg-accent/20 btn-circle'
-			},
-			isLoading: {
-				true: 'flex gap-2'
-			},
-			animated: {
-				true: 'hover:-translate-y-0.5'
-			},
-			size: {
-				sm: 'btn-sm'
+			defaultVariants: {
+				intent: 'primary'
 			}
-		},
-		defaultVariants: {
-			intent: 'primary'
 		}
-	});
+	);
 </script>
 
 {#if to !== ''}
@@ -64,6 +71,7 @@
 		{type}
 		disabled={disabled || isLoading}
 		class={`${buttonStyles({ ...variants, disabled: disabled || isLoading })} ${$$props.class}`}
+		on:click
 	>
 		<slot />
 		{#if isLoading}
