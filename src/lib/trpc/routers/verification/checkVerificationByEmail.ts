334464a0dc135f2f8bb8a8c73c710effac41098b
Lucas <47724710+fell-lucas@users.noreply.github.com>
2023-02-25 23:59:20 -0300

    fix: 🐛 correct error message if user do not exist on signin (#42)

--- src/lib/trpc/routers/verification/checkVerificationByEmail.ts
+++ src/lib/trpc/routers/verification/checkVerificationByEmail.ts
@@ -6,27 +6,32 @@ import { TRPCError } from '@trpc/server';
 export const checkVerificationByEmail = publicProcedure
 	.input(checkVerificationByEmailSchema)
 	.query(async ({ input }) => {
-		const userVerifications = (
-			await prisma.user.findUnique({
-				select: {
-					Verification: {
-						where: {
-							type: input.type
-						}
+		const user = await prisma.user.findUnique({
+			select: {
+				Verification: {
+					where: {
+						type: input.type
 					}
-				},
-				where: { email: input.email }
-			})
-		)?.Verification;
+				}
+			},
+			where: { email: input.email }
+		});
 
-		if (!userVerifications || !userVerifications.length) {
+		if (!user) {
+			throw new TRPCError({
+				message: `exceptions.users.user-not-found`,
+				code: 'BAD_REQUEST'
+			});
+		}
+
+		if (!user.Verification || !user.Verification.length) {
 			throw new TRPCError({
 				message: `exceptions.users.verifications.${input.type.toLowerCase()}.no-verifications`,
 				code: 'BAD_REQUEST'
 			});
 		}
 
-		const verified = userVerifications.filter((verification) => verification.isVerified);
+		const verified = user.Verification.filter((verification) => verification.isVerified);
 
 		if (verified.length > 0) {
 			return true;
--- src/routes/(e2e)/auth-dialog/signin.e2e.ts
+++ src/routes/(e2e)/auth-dialog/signin.e2e.ts
@@ -1,4 +1,4 @@
-import test from '@playwright/test';
+import test, { expect } from '@playwright/test';
 
 test.use({
 	locale: 'en-US',
@@ -9,12 +9,22 @@ test.use({
 });
 
 test.describe('auth dialog', () => {
-	test('signs in', async ({ page }) => {
+	test('signs in successfully', async ({ page }) => {
 		await page.goto('/', { waitUntil: 'networkidle' });
 		await page.getByTestId('nav-signin-btn').click();
 		await page.getByTestId('signin-email-input').fill('ext-signin@mailinator.com');
 		await page.getByTestId('signin-password-input').fill('#1Abcdef');
 		await page.getByTestId('signin-submit-button').click();
 		await page.getByTestId('menu-trigger-user-menu').click();
+	});
+
+	test('fails to sign in', async ({ page }) => {
+		await page.goto('/', { waitUntil: 'networkidle' });
+		await page.getByTestId('nav-signin-btn').click();
+		await page.getByTestId('signin-email-input').fill('ext-signin-invalid@mailinator.com');
+		await page.getByTestId('signin-password-input').fill('#1Abcdef');
+		await page.getByTestId('signin-submit-button').click();
+
+		await expect(page.getByTestId('toast-body')).toHaveText('User not found');
 	});
 });
